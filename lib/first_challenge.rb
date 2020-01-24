d228f185bae5e609ee343d88656d0e2fbe458abf
victor tim <technicalpro@securemedia.ch>
2020-01-24 22:24:12 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,14 +1,14 @@
-def bonus
+def first_challenge
   epic_tragedy = {
-     { :montague =>{},
+      {:montague =>{},
       :patriarch =>{},
       :matriarch =>{},
       :hero =>{},
       :hero_friends =>{},
 
-      }:capulet =>{},
+      },{:capulet =>{},
       :patriarch =>{},
       :matriarch =>{},
       :heroine =>{},
-      :heroine_friends =>{}}
+      :heroine_friends =>{},}}
 end
